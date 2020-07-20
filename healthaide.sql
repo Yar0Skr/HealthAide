a5c9867413f5e043b104a1621f17acf8fd94ed57
Yaroslav Skrypnyk <yar.skrypnyk@gmail.com>
2020-07-20 18:20:13 +0300

    debug

---
+++ healthaide.sql
@@ -0,0 +1,97 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.1.37-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Дамп структуры базы данных healthaide
+CREATE DATABASE IF NOT EXISTS `healthaide` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `healthaide`;
+
+-- Дамп структуры для таблица healthaide.blog
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `header` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `short_info` text COLLATE utf8_unicode_ci,
+  `text` text COLLATE utf8_unicode_ci,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `alt_tag` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `order_number` int(11) DEFAULT NULL,
+  `url_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `read_time` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `is_delete` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url_name` (`url_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Дамп данных таблицы healthaide.blog: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `blog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog` ENABLE KEYS */;
+
+-- Дамп структуры для таблица healthaide.locations
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Дамп данных таблицы healthaide.locations: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` (`id`, `url`, `name`) VALUES
+	(1, 'boston', 'Boston'),
+	(2, 'new-york', 'New York'),
+	(3, 'ukraine', 'Ukraine');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+
+-- Дамп структуры для таблица healthaide.migration
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) COLLATE utf8_bin NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Дамп данных таблицы healthaide.migration: ~6 rows (приблизительно)
+/*!40000 ALTER TABLE `migration` DISABLE KEYS */;
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+	('m000000_000000_base', 1593946662),
+	('m200425_140718_users', 1593946940),
+	('m200705_105103_create_blog_table', 1593946663),
+	('m200705_105118_create_locations_table', 1593946940),
+	('m200708_133637_create_locations_table', 1594215500),
+	('m200717_111735_add_name_field_to_location', 1594984756);
+/*!40000 ALTER TABLE `migration` ENABLE KEYS */;
+
+-- Дамп структуры для таблица healthaide.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `is_delete` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `auth_type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Дамп данных таблицы healthaide.users: ~1 rows (приблизительно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `is_delete`, `created_at`, `updated_at`, `auth_type`) VALUES
+	(1, 'admin', 'admin', '$2y$13$o/CVFFYPqX9MlgbG9fHZL.N47H6GKNcbPdljSrHixSay7rPFS0Ed2', 0, '2020-07-05 14:02:20', '2020-07-08 15:24:21', 1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
